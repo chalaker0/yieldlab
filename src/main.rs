b696984c021608a29a251f6642650b1620e988da
chalaker0 <marianosaona12@gmail.com>
2025-12-25 09:33:40 -0500

    Font and spacing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use crate::components::input::Input;
-use crate::components::label::Label;
 use dioxus::prelude::*;
 
 mod components;
@@ -16,13 +15,13 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Hero {}
+        Header {}
+        Inputs {}
     }
 }
 
 #[component]
-pub fn Hero() -> Element {
-    let mut children = use_signal(String::new);
+pub fn Header() -> Element {
     rsx! {
         // Header
         div {
@@ -35,14 +34,51 @@ pub fn Hero() -> Element {
             }
         }
 
+        div { class: "container" }
+    }
+}
+
+#[component]
+pub fn Inputs() -> Element {
+    let mut pv = use_signal(String::new);
+    let mut r = use_signal(String::new);
+    let mut fv = use_signal(String::new);
+    let mut t = use_signal(String::new);
+
+    rsx!{
+        // Instrucciones
+        div {"Usa los datos segun el credito que quieres conseguir!"}
+        div { class: "small_container"}
+
+        // Inputs
         div {
             display: "flex", flex_direction: "column", gap: ".5rem",
-            Label { html_for: "name" , "Name" }
+            Input {
+                oninput: move |e: FormEvent| pv.set(e.value()),
+                placeholder: "Cuota inicial",
+                value: pv,
+                class: "input",
+            }
 
             Input {
-                oninput: move |e: FormEvent| children.set(e.value()),
-                placeholder: "PV",
-                value: children,
+                oninput: move |e: FormEvent| r.set(e.value()),
+                placeholder: "TCEA", // Poner un component Hover Card checkearlo y tal vez elegir
+                                     // entre TCEA TEA o TREA (explicar bien)
+                value: r,
+                class: "input",
+            }
+
+            Input {
+                oninput: move |e: FormEvent| fv.set(e.value()),
+                placeholder: "Monto solicitado",
+                value: fv,
+                class: "input",
+            }
+
+            Input {
+                oninput: move |e: FormEvent| t.set(e.value()),
+                placeholder: "Plazo", //Despues poner una opcion para elegir meses o anios
+                value: t,
                 class: "input",
             }
         }
